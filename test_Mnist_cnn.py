37fdd9273e17b9a800314b7a5079a2d98a382b95
kadygithub <workaki@gmail.com>
2019-05-13 11:28:56 -0400

    Update test_Mnist_cnn.py

--- test_Mnist_cnn.py
+++ test_Mnist_cnn.py
@@ -1,30 +1,31 @@
-#from __future__ import absolute_import
-#from __future__ import division
-#from __future__ import print_function
-
-#import numpy
-#import os
-#import tensorflow as tf
-#import tensorflow.contrib.slim as slim
-#from tensorflow.examples.tutorials.mnist import input_data
-
-#import mnist_data
-#import cnn_model
-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from scipy import ndimage
+from six.moves import urllib
 # user input
-from argparse import ArgumentParser
+from argparse import ArgumentParser 
+
+import numpy
+import os
+import tensorflow as tf
+import gzip
+
+import Data_Mnist
+import model_cnn
+
 
 # refernce argument values
 MODEL_DIRECTORY = "model"
 TEST_BATCH_SIZE = 5000
-ENSEMBLE = True
+
 
 # build parser
 def build_parser():
     parser = ArgumentParser()
 
     parser.add_argument('--model-dir',
-                        dest='model_directory', help='directory where model to be tested is stored',
+                        dest='model_directory', help='directory  of the model to be tested',
                         metavar='MODEL_DIRECTORY', required=True)
     parser.add_argument('--batch-size', type=int,
                         dest='batch_size', help='batch size for test',
@@ -38,12 +39,12 @@ def test_org(model_directory, batch_size):
     d=Data_Mnist()
     train_data,train_labels,train_total_data, train_size, validation_data, validation_labels, test_data, test_labels=d.Produce_data(data_augmentation_Flag=False)
     # Import data
-    PIXEL_DEPTH = d.PIXEL_DEPTH
+    
     
 
     # tf Graph input
     x = tf.placeholder(tf.float32, [None, 784])
-    y_ = tf.placeholder(tf.float32, [None, 10])  # answer
+    y_ = tf.placeholder(tf.float32, [None, 10])  
     y = cnn_net(x)
     
 
@@ -55,7 +56,6 @@ def test_org(model_directory, batch_size):
     saver = tf.train.Saver()
 
     # Calculate accuracy for all mnist test images
-    # Calculate accuracy for all mnist test images
     test_size = test_labels.shape[0]
     
     total_batch = int(test_size / batch_size)
@@ -63,7 +63,7 @@ def test_org(model_directory, batch_size):
 
     saver.restore(sess, model_directory)
 
-    acc_buffer = []
+    acc_buf = []
 
     # Loop over all batches
     for i in range(total_batch):
@@ -76,9 +76,9 @@ def test_org(model_directory, batch_size):
 
         correct_prediction = numpy.equal(numpy.argmax(y_final, 1), numpy.argmax(batch_ys, 1))
 
-        acc_buffer.append(numpy.sum(correct_prediction) / batch_size)
+        acc_buf.append(numpy.sum(correct_prediction) / batch_size)
 
-    print("test accuracy for the stored model: %g" % numpy.mean(acc_buffer))
+    print("Test accuracy for the stored model: %g" % numpy.mean(acc_buf))
 if __name__ == '__main__':
     # Parse argument
     parser = build_parser()
@@ -86,4 +86,4 @@ if __name__ == '__main__':
     ensemble = options.ensemble
     model_directory = options.model_directory
     batch_size = options.batch_size
-    test(model_directory+'/model.ckpt',batch_size)  # test with test data given by tensorflow.examples.tutorials.mnist.input_data()
+    test(model_directory+'/model.ckpt',batch_size)  
